b5736c8d3899a9f2e511857e579b839092cc674d
hirano <hirano@diatrend.co.jp>
2016-04-15 11:06:24 +0900

    Sixth-2

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ Created on 2016/03/03
 
 @author: hirano
 """
+import sys
 import os
 import pygame
 import pygameuic as ui  # @UnresolvedImport
@@ -15,6 +16,13 @@ import mytheme
 # os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
 if __name__ == '__main__':
+    param = sys.argv
+    if len(param) > 1:
+        if param[1] == "-tft":
+            os.putenv('SDL_FBDEV', '/dev/fb1')
+            os.putenv('SDL_MOUSEDRV', 'TSLIB')
+            os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
+            pygame.mouse.set_visible(False)
     ui.init('pygameui ', (320, 240))
 #     pygame.mouse.set_visible(False)
     mytheme.set_theme()
